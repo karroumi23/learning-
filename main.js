dcf95851d67e68f33a42205b85329cd0037b6772
karroumi <anasskarroumi2016@gmail.com>
2024-02-03 21:10:22 +0100

    add social media

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//----------------- start dropdown
 var menuId = document.getElementById("menuId");
 
-function open_closs(){
+function open_closs() {
   menuId.classList.toggle("mystyle");
 }
+//----------------- end dropdown
